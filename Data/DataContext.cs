a2099a17c87a9914b7c69b66ce4f01aaa46be9f4
Fatema Alteneiji <fatema.alteneiji@maqta.ae>
2022-11-14 14:51:07 +0400

    Student Registration

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -18,7 +18,7 @@ namespace student_registration.Data
             modelBuilder.Entity<Level>().HasData(
              new Level { Id = 1, Code = "PHD", Description = "A Doctor of Philosophy is an academic degree focused on original research, data analysis, and the evaluation of theory." },
              new Level { Id = 2, Code = "PG", Description = "A Postgraduate degree is a type of qualification that is completed after an undergraduate degree." },
-             new Level { Id = 3, Code = "G", Description = "A Graduate degree is an advanced academic degree in a specialized field of study, pursued after one has already obtained a bachelor’s degree." },
+             new Level { Id = 3, Code = "G", Description = "A Graduate degree is an advanced academic degree in a specialized field of study, pursued after one has already obtained a bachelorï¿½s degree." },
              new Level { Id = 4, Code = "D", Description = "A diploma is a certificate awarded by an educational institution to a student for completing a specific course and passing the examination." }
             );
         }
